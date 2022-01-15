c78830d90668e32d531e32a875e2d06f585e4cc7
sjoongh <az45687@naver.com>
2022-01-15 17:32:56 +0900

    JSX study

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,16 +11,85 @@ import reportWebVitals from './reportWebVitals';
 // const name = 'shin joong ho';
 // const element = <h1>Hello, {name}</h1>;
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName
-}
+// JSX test 2
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName
+// }
+
+// const user = {
+//   firstName: 'Harper',
+//   lastName: 'Perez'
+// };
+
+// const element = (
+//   <h1>
+//     Hello, {getGreeting(user)}!
+//   </h1>
+// );
+
+// JSX도 표현식이다. 컴파일시 결국 JS객체로 인식됨
+// function getGreeting(user) {
+//   if (user) {
+//     return <h1>Hello, {formatName(user)}!</h1>;
+//   }
+//   return <h1>Hello, Stranger.</h1>;
+// }
+
+// JSX 속성 정의
+// 어트리뷰트에 따옴표를 이용해 문자열 리터럴을 정의할 수 있음.
+// const element1 = <div tabIndex="0"></div>;
+
+// 중괄호를 사용하여 JS표현식 삽입
+// user변수에 있는 avatarUrl에 접근
+// const element2 = <img src={user.avatarUrl}></img>;
+
+// 동일한 어트리뷰트에 위의 두가지 방법중 하나만 사용해야함
+
+// JSX는 JS에 가까운 언어이기에 camelCase프로퍼티 명명 규칙을 사용.
+// 예를 들어 JSX에서 class는 className가 되고 tabindex는 tabIndex가 된다.
+
+// JSX는 주입 공격을 방지
+// but 사용자 입력을 삽입하는 것은 안전하다.
+// const element = <h1>{title}</h1>; --> {title}이 입력됨
+
+// 기본적으로 react DOM은 jsx에 삽입된 모든 값을 렌터링 하기 전에 이스케이프 하므로 
+// 애플리케이션에서 명시적으로 작성되지 않은 내용은 주입되지 않음.
+// 때문에 모든 항목은 렌더링 되기 전에 문자열로 변환된다.
+// 이스케이프 : 특정 문자를 HTML로 변환하는 행위
+
+// JSX는 객체를 표현합니다.
+// Bable은 JSX를 React.createElement() 호출로 컴파일한다.
+
+// React.createElement()는 버그가 없는 코드를 작성하는데 도움이 되도록 몇 가지 검사를 수행.
+// 이러한 객체를 React엘리먼트라고 하며, 화면에서 보고 싶은 것을 나타내는 표현,
+
+// 다음 두 예시는 동일한 예시이다.
+// const element = (
+//   <h1 className='greeting'>
+//     Hello, world
+//   </h1>
+// );
+
+// const element = React.createElement(
+//   'h1',
+//   {className: 'greeting'},
+//   'Hello, world!'
+// );
+
+const element = {
+  type: 'h1',
+  props: {
+    className: 'greeting',
+    children: 'Hello, world!'
+  }
+};
 
 ReactDOM.render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
   // <h1>Hello world!</h1>,
-  // element, --> JSX test 1
+  element,
   document.getElementById('root')
 );
 
@@ -28,3 +97,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
