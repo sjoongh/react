6e36b5b463610d3cbc4087eaabe2ebece0533226
sjoongh <az45687@naver.com>
2022-01-15 16:28:52 +0900

    JSX start

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// JSX에 표현식 포함하기 --> JSX test 1
+// {name}이라는ㅇ 변수를 선언한후 JSX안에서 사용
+// JSX의 중괄호 안에는 유효한 모든 JS표현식을 넣을 수 있음.
+// const name = 'shin joong ho';
+// const element = <h1>Hello, {name}</h1>;
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  // <h1>Hello world!</h1>,
+  // element, --> JSX test 1
   document.getElementById('root')
 );
 
